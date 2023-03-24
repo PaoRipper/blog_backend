36095dc64750821896433f9629746bcef092ac83
Tossakorn <Paoripper01@gmail.com>
2023-03-24 17:21:38 +0700

    initial commit

---
+++ db_config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database", err.stack);
+    return;
+  }
+  console.log("Connected to database with ID: ", conn.threadId);
+});
+
+module.exports = conn;
---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const conn = require('./db_config')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// GET all users
+app.get("/users", (req, res) => {
+  conn.query("SELECT * FROM users", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// POST a new user
+app.post("/user", (req, res) => {
+  const { username, password, email } = req.body;
+
+  // Check if user with same email already exists
+  conn.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+    if (err) throw err;
+
+    if (results.length > 0) {
+      // This email already exist
+      return res.status(409).send({ message: "Email already exist" });
+    }
+    // Insert new user
+    conn.query(
+      "INSERT INTO users (Username, Password, Email) VALUES (?, ?, ?)",
+      [username, password, email],
+      (err, results) => {
+        if (err) throw err;
+        const user = { message: "success", data: { username, email } };
+        res.json(user);
+      }
+    );
+  });
+});
+
+// GET ALL posts
+app.get("/posts", (req, res) => {
+  conn.query("SELECT * FROM posts", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// POST a new post
+app.post("/post", (req, res) => {
+  const { content, userID } = req.body;
+
+  conn.query(
+    "INSERT INTO posts (Content, UserID) VALUES (?, ?)",
+    [content, userID],
+    (err, results) => {
+      if (err) throw err;
+      const post = { message: "success", data: { content, userID } };
+      res.json(post);
+      // res.status(200).send({ message: "Insert successfully" });
+    }
+  );
+});
+
+// GET ALL comments
+app.get("/comments", (req, res) => {
+  conn.query("SELECT * FROM comments", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// POST a new comment
+app.post("/comment", (req, res) => {
+  const { content, userID, postID } = req.body;
+
+  conn.query(
+    "INSERT INTO comments (Content, UserID, PostID) VALUES (?, ?, ?)",
+    [content, userID, postID],
+    (err, results) => {
+      if (err) throw err;
+      const comment = { message: "success", data: { content, userID, postID } };
+      res.json(comment);
+    }
+  );
+});
+
+app.listen(process.env.DB_PORT, () => {
+  console.log(`Running on port ${process.env.DB_PORT}`);
+});
