d55c185033cb59160df5e5cd6dfd264d34fb3221
Tossakorn <Paoripper01@gmail.com>
2023-04-17 22:21:13 +0700

    fix

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 const domain = process.env.DOMAIN;
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
-const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const serverPort = process.env.SERVER_PORT;
 const lineAccessToken = process.env.LINE_ACCESS_TOKEN;
 
